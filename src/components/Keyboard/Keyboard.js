c9f97d3e2ac2269bd1b68e0db115090438bb8bf0
Marian <marianlopig@gmail.com>
2022-04-28 02:04:56 +0200

    disable keyboard when calling

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -3,7 +3,7 @@ import Context from "../../context/Context";
 import Key from "../Key/Key";
 
 const Keyboard = () => {
-  const { setDisplay, display } = useContext(Context);
+  const { setDisplay, display, calling } = useContext(Context);
 
   const clickKey = (number) => {
     if (display.length < 9) {
@@ -20,37 +20,82 @@ const Keyboard = () => {
     <div className="keyboard-container">
       <ol className="keyboard">
         <li>
-          <Key number={1} action={() => clickKey(1)} />
+          <Key
+            number={1}
+            action={() => clickKey(1)}
+            disabled={calling ? true : false}
+          />
         </li>
         <li>
-          <Key number={2} action={() => clickKey(2)} />
+          <Key
+            number={2}
+            action={() => clickKey(2)}
+            disabled={calling ? true : false}
+          />
         </li>
         <li>
-          <Key number={3} action={() => clickKey(3)} />
+          <Key
+            number={3}
+            action={() => clickKey(3)}
+            disabled={calling ? true : false}
+          />
         </li>
         <li>
-          <Key number={4} action={() => clickKey(4)} />
+          <Key
+            number={4}
+            action={() => clickKey(4)}
+            disabled={calling ? true : false}
+          />
         </li>
         <li>
-          <Key number={5} action={() => clickKey(5)} />
+          <Key
+            number={5}
+            action={() => clickKey(5)}
+            disabled={calling ? true : false}
+          />
         </li>
         <li>
-          <Key number={6} action={() => clickKey(6)} />
+          <Key
+            number={6}
+            action={() => clickKey(6)}
+            disabled={calling ? true : false}
+          />
         </li>
         <li>
-          <Key number={7} action={() => clickKey(7)} />
+          <Key
+            number={7}
+            action={() => clickKey(7)}
+            disabled={calling ? true : false}
+          />
         </li>
         <li>
-          <Key number={8} action={() => clickKey(8)} />
+          <Key
+            number={8}
+            action={() => clickKey(8)}
+            disabled={calling ? true : false}
+          />
         </li>
         <li>
-          <Key number={9} action={() => clickKey(9)} />
+          <Key
+            number={9}
+            action={() => clickKey(9)}
+            disabled={calling ? true : false}
+          />
         </li>
         <li>
-          <Key number={0} action={() => clickKey(0)} />
+          <Key
+            number={0}
+            action={() => clickKey(0)}
+            disabled={calling ? true : false}
+          />
         </li>
         <li>
-          <Key className="big" number={"delete"} action={clickDelete} />
+          <Key
+            className="big"
+            number={"delete"}
+            action={clickDelete}
+            disabled={calling ? true : false}
+          />
         </li>
       </ol>
     </div>
